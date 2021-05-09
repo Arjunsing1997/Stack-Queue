7bcd9ed5bb8db2494ebc1fe8effc20f242920407
Arjunsing1997 <acchur1997@gmail.com>
2021-05-09 17:38:41 +0530

    UC3 creating a Queue

--- Stack-Queue/Stack-Queue/Program.cs
+++ Stack-Queue/Stack-Queue/Program.cs
@@ -12,16 +12,24 @@ namespace Stack_Queue
         {
             Console.WriteLine("Welcome to Stack Program....");
 
-            Console.WriteLine("Stack");//Print 
-            Stack stack = new Stack(); //create object of Stack class
-            stack.Push(56); //call Push method Push element in stack 
-            stack.Push(30);
-            stack.Push(70);
+            //Console.WriteLine("Stack");//Print 
+            //Stack stack = new Stack(); //create object of Stack class
+            //stack.Push(56); //call Push method Push element in stack 
+            //stack.Push(30);
+            //stack.Push(70);
 
-            stack.Peek(); //call Peek method print top of stack //UC2
-            stack.Pop(); //call Pop method and remove top of the stack //UC2
-            stack.isEmpty(); //call isEmpty method remove all elements in stack//UC2
-            stack.Display();//call Display method Display element in stack 
+            //stack.Peek(); //call Peek method print top of stack //UC2
+            //stack.Pop(); //call Pop method and remove top of the stack //UC2
+            //stack.isEmpty(); //call isEmpty method remove all elements in stack//UC2
+            //stack.Display();//call Display method Display element in stack 
+
+            Console.WriteLine("Queue"); //Print 
+            Queue queue = new Queue();  //create object of Queue class
+            queue.Enqueue(56); //call method add Element in queue //UC1
+            queue.Enqueue(30); //call method add Element in queue //UC1
+            queue.Enqueue(70); //call method add Element in queue //UC1
+            queue.Display(); //call Display method Display element in Queue  //UC1
+            queue.Peek(); //call Display method Display Top of the element in Queue  //UC1
             Console.ReadLine();
         }
     }
