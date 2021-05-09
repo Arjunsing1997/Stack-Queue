fddd268095162d1703411c61900a81fec600c53d
Arjunsing1997 <acchur1997@gmail.com>
2021-05-09 17:29:11 +0530

    UC1 created stack

--- Stack-Queue/Stack-Queue/Program.cs
+++ Stack-Queue/Stack-Queue/Program.cs
@@ -11,6 +11,14 @@ namespace Stack_Queue
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack Program....");
+
+            Console.WriteLine("Stack");//Print 
+            Stack stack = new Stack(); //create object of Stack class
+            stack.Push(56); //call Push method Push element in stack 
+            stack.Push(30);
+            stack.Push(70);
+            stack.Display();//call Display method Display element in stack 
+            Console.ReadLine();
         }
     }
 }
