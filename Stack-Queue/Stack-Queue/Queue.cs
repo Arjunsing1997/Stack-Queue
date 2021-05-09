06a20ff5be1ab9ec36baf6f0696c5311227a2bef
Arjunsing1997 <acchur1997@gmail.com>
2021-05-09 17:43:00 +0530

    UC4 Dequeue and isEmpty operation

--- Stack-Queue/Stack-Queue/Queue.cs
+++ Stack-Queue/Stack-Queue/Queue.cs
@@ -40,6 +40,28 @@ namespace Stack_Queue
             }
             return temp;
         }
+        public void Dequeue() //create Dequeue method to delete element
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Queue is Empty");
+            }
+            Console.WriteLine($"Removing the beginning { this.head.data } Element from the Queue"); //print
+            head = head.next; //passing the address of 2nd element to the head, hence head will directly point to 2nd element and 1st element delete
+            Display(); //call Display method
+
+        }
+        public void IsEmpty() //create isEmpty method to delete all element from Queue
+        {
+            Node temp = head;
+            while (temp != null)
+            {
+                Peek(); //call Peek method
+                Dequeue(); //call Dequeue method
+                temp = temp.next;
+            }
+            Display(); //call Display MEthod
+        }
 
         public void Display()
         {
